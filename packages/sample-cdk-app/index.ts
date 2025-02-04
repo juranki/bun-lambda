c865b245ad20c0281206cfa654daeabc3bac5723
Juhani Rankimies <juhani@juranki.com>
2025-02-04 21:42:39 +0200

    fix: remove acct nr

--- packages/sample-cdk-app/index.ts
+++ packages/sample-cdk-app/index.ts
@@ -3,17 +3,8 @@ import { SampleCdkAppStack } from './stack/sample-cdk-app-stack';
 
 const app = new cdk.App();
 new SampleCdkAppStack(app, 'SampleCdkAppStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  env: { account: '354918398605', region: 'eu-north-1' },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  env: {
+    account: process.env.CDK_TARGET_ACCOUNT,
+    region: process.env.CDK_TARGET_REGION,
+  },
 });
