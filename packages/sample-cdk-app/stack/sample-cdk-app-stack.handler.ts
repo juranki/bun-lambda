b863e00fe57cedbde699bce8891211cbdaff3622
Juhani Rankimies <juhani@juranki.com>
2025-02-06 17:53:08 +0200

    feat: add function urls and hono handler

--- packages/sample-cdk-app/stack/sample-cdk-app-stack.handler.ts
+++ packages/sample-cdk-app/stack/sample-cdk-app-stack.handler.ts
@@ -1,14 +1,6 @@
-import type { Context } from 'aws-lambda';
 import { startRuntime } from 'bun-lambda-boostrap';
+import { handler } from '../app';
 
-// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-async function handler(event: any, context: Context) {
-  console.log('event', JSON.stringify(event, null, 2));
-  console.log('context', JSON.stringify(context, null, 2));
-  return { message: 'Hello from Bun AWS Lambda!' };
-}
-
-// if this is main module, start runtime
 if (require.main === module) {
   startRuntime(handler).catch(console.error);
 }
--- packages/sample-cdk-app/stack/sample-cdk-app-stack.ts
+++ packages/sample-cdk-app/stack/sample-cdk-app-stack.ts
@@ -2,19 +2,22 @@ import { BunFunction } from 'bun-lambda-construct';
 import * as cdk from 'aws-cdk-lib';
 import type { Construct } from 'constructs';
 import { join } from 'node:path';
-import { Architecture } from 'aws-cdk-lib/aws-lambda';
+import { Architecture, FunctionUrlAuthType } from 'aws-cdk-lib/aws-lambda';
 
 export class SampleCdkAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new BunFunction(this, 'handler-arm', {
+    const arm = new BunFunction(this, 'handler-arm', {
       entry: join(__dirname, 'sample-cdk-app-stack.handler.ts'),
       architecture: Architecture.ARM_64,
     });
 
-    new BunFunction(this, 'handler-x86', {
+    const x86 = new BunFunction(this, 'handler-x86', {
       entry: join(__dirname, 'sample-cdk-app-stack.handler.ts'),
     });
+
+    arm.function.addFunctionUrl({ authType: FunctionUrlAuthType.NONE });
+    x86.function.addFunctionUrl({ authType: FunctionUrlAuthType.NONE });
   }
 }
